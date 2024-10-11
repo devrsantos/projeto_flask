ae67577960a4b4096a307e3b6c7f1ca23d484ed1
Renan Augusto dos Santos <devrsantos@gmail.com>
2024-10-11 20:43:27 -0300

    feat: implementação da funcionalidade de busca de usuário por ID

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.models import User
 from app import db
 from app.repositories import UserRepository 
@@ -19,3 +19,12 @@ def cadastrar_usuario():
 def listar_usuarios():
     usuarios = user_repository.listar_usuarios()
     return render_template('listar.html', usuarios=usuarios)
+
+@bp.route('/usuario/<int:id>')
+def obter_usuario_por_id(id):
+    usuario = user_repository.obter_usuario_por_id(id)
+    if usuario:
+        return render_template('usuario.html', usuario=usuario)
+    else:
+        flash('Usuario não encontrado.', 'danger')
+        return redirect(url_for('routes.listar_usuarios'))
