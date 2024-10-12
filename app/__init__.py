7305a97ee32687b2d5ae6635387766c3eed0315d
Renan Augusto dos Santos <devrsantos@gmail.com>
2024-10-11 21:14:50 -0300

    feat: implementação do CRUD completo para usuários

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import SECRET_KEY
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config['SECRET_KEY'] = SECRET_KEY
     from config import SQLALCHEMY_DATABASE_URI
       
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///meu_banco_de_dados.db'  
+SECRET_KEY = b'@\xda9I\xbf_y\xa93\x18\xff\xde*\xe0\x154G2y\x9f\x92sa}'
